d59513d2abe1d53d2aeba2c623fecbba7ba06619
Alanv3514 <Alanv3514@gmail.com>
2025-06-26 17:10:27 -0300

    feat:Ejercicio 2 Clase Servicio

--- Ejercicio2/Form1.cs
+++ Ejercicio2/Form1.cs
@@ -1,10 +1,80 @@
+using Ejercicio2.CS;
+using Ejercicio2.CS.Modelos;
+
 namespace Ejercicio2
 {
     public partial class Form1 : Form
     {
+        ServicioVehiculos SVehiculos = new ServicioVehiculos();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            Fdatos fdatos = new Fdatos();
+            if (fdatos.ShowDialog() == DialogResult.OK)
+            {
+                if (SVehiculos.BuscarVehiculo(fdatos.tBpatente.Text) == -1)
+                {
+                    SVehiculos.CrearVehiculo(fdatos.tBpatente.Text, Convert.ToDouble(fdatos.tBkilometraje.Text));
+
+                    this.listBoxPatentes.Items.Add(fdatos.tBpatente.Text);
+
+                    this.btnCargarViaje.Enabled = true;
+                    this.btnResumenFinal.Enabled = true;
+
+                    if (SVehiculos.VerCantidadVehiculos() == 15)
+                    {
+                        this.btnRegistrar.Enabled = false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error, vehiculo repetido.");
+                }
+                
+            }
+
+        }
+
+        private void btnCargarViaje_Click(object sender, EventArgs e)
+        {
+            Fdatos fdatos = new Fdatos();
+
+
+            if (fdatos.ShowDialog() == DialogResult.OK)
+            {
+                if ((SVehiculos.BuscarVehiculo(fdatos.tBpatente.Text)) != -1)
+                {
+                    SVehiculos.CargarViaje(fdatos.tBpatente.Text, Convert.ToDouble(fdatos.tBkilometraje.Text));
+                    this.labelCantViajes.Text = $"{SVehiculos.VerCantidadViajes()}";
+                }
+                else
+                {
+                    MessageBox.Show("Error, no se encuentra el vehiculo.");
+                }
+                
+            }
+
+        }
+
+        private void btnResumenFinal_Click(object sender, EventArgs e)
+        {
+            Fresumen fresumen = new Fresumen();
+            SVehiculos.Ordenar();
+            for (int i = 0; i < SVehiculos.VerCantidadVehiculos(); i++)
+            {
+                fresumen.listBoxResumen.Items.Add($"Kilometraje:{SVehiculos.VerKilometraje(i),10:f2} - Patentes {SVehiculos.VerPatente(i)}");
+            }
+            fresumen.listBoxResumen.Items.Add($"Promedio Kilometros:{SVehiculos.VerPromedioKm(),10:f2} - Viajes Totales: {SVehiculos.VerCantidadViajes()}");
+            fresumen.ShowDialog();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
---
+++ Ejercicio2/Modelos/ServicioVehiculos.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.CS.Modelos
+{
+    internal class ServicioVehiculos
+    {
+        private string[] patentes = new string[15];
+        private double[] kilometrajes = new double[15];
+        private int vehiculos = 0;
+        private int cantidadViajes = 0;
+
+        public void CrearVehiculo(string pat, double km)
+        {
+            if ((vehiculos < 15) && (BuscarVehiculo(pat) ==-1))
+            {
+                patentes[vehiculos] = pat;
+                kilometrajes[vehiculos] = km;
+                vehiculos ++;
+            }
+
+        }
+
+        public void CargarViaje(string pat,double km)
+        {
+            int idx = BuscarVehiculo(pat);
+            if (idx != -1)
+            {
+                kilometrajes[idx] += km;
+                cantidadViajes++;
+            }
+        }
+
+        public double VerKilometraje(int idx)
+        {
+            return kilometrajes[idx];
+        }
+        public string VerPatente(int idx)
+        {
+            return patentes[idx];
+        }
+        public int VerCantidadViajes()
+        {
+            return cantidadViajes;
+        }
+        public int VerCantidadVehiculos()
+        {
+            return vehiculos;
+        }
+        public int BuscarVehiculo(string pat)
+        {
+            int res = -1;
+            for (int i = 0; i < vehiculos; i++)
+            {
+                if(patentes[i] == pat)
+                    res= i;
+            }
+            return res;
+        }
+
+        public void Ordenar()
+        {
+            double DAux;
+            string SAux;
+
+            for(int i = 0;i < vehiculos - 1; i++)
+            {
+                for(int j = i+1; j < vehiculos; j++)
+                {
+                    if (kilometrajes[i] > kilometrajes[j])
+                    {
+                        DAux = kilometrajes[j];
+                        kilometrajes[j] = kilometrajes[i];
+                        kilometrajes[i] = DAux;
+
+                        SAux = patentes[j];
+                        patentes[j] = patentes[i];
+                        patentes[i] = SAux;
+                    }
+                }
+            }
+        }
+
+        public double VerPromedioKm()
+        {
+            double acum = 0;
+            for (int i = 0; i < vehiculos; i++)
+            {
+                acum += kilometrajes[i];
+            }
+            return (acum / VerCantidadVehiculos());
+        }
+    }
+}
