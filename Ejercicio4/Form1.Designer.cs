48f149ac0f59facdf602550844a4d9b1ecc3a5d3
Alanv3514 <Alanv3514@gmail.com>
2025-06-26 18:03:28 -0300

    feat: Creacion del Ejercicio 4 aun sin implementar clase servicio

---
+++ Ejercicio4/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Ejercicio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnCargarViaje = new Button();
+            btnResumenFinal = new Button();
+            btnSalir = new Button();
+            listBoxPatentes = new ListBox();
+            label1 = new Label();
+            labelCantViajes = new Label();
+            btnRegistrar = new Button();
+            SuspendLayout();
+            // 
+            // btnCargarViaje
+            // 
+            btnCargarViaje.Enabled = false;
+            btnCargarViaje.Location = new Point(32, 89);
+            btnCargarViaje.Name = "btnCargarViaje";
+            btnCargarViaje.Size = new Size(82, 46);
+            btnCargarViaje.TabIndex = 1;
+            btnCargarViaje.Text = "Cargar Viaje";
+            btnCargarViaje.UseVisualStyleBackColor = true;
+            btnCargarViaje.Click += btnCargarViaje_Click;
+            // 
+            // btnResumenFinal
+            // 
+            btnResumenFinal.Enabled = false;
+            btnResumenFinal.Location = new Point(32, 141);
+            btnResumenFinal.Name = "btnResumenFinal";
+            btnResumenFinal.Size = new Size(82, 46);
+            btnResumenFinal.TabIndex = 2;
+            btnResumenFinal.Text = "Resumen Final";
+            btnResumenFinal.UseVisualStyleBackColor = true;
+            btnResumenFinal.Click += btnResumenFinal_Click;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(32, 193);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(82, 46);
+            btnSalir.TabIndex = 3;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // listBoxPatentes
+            // 
+            listBoxPatentes.FormattingEnabled = true;
+            listBoxPatentes.ItemHeight = 15;
+            listBoxPatentes.Location = new Point(175, 37);
+            listBoxPatentes.Name = "listBoxPatentes";
+            listBoxPatentes.Size = new Size(318, 154);
+            listBoxPatentes.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(175, 209);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Viajes:";
+            // 
+            // labelCantViajes
+            // 
+            labelCantViajes.AutoSize = true;
+            labelCantViajes.Location = new Point(234, 209);
+            labelCantViajes.Name = "labelCantViajes";
+            labelCantViajes.Size = new Size(12, 15);
+            labelCantViajes.TabIndex = 6;
+            labelCantViajes.Text = "-";
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.Location = new Point(32, 37);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(82, 46);
+            btnRegistrar.TabIndex = 7;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = true;
+            btnRegistrar.Click += btnRegistrar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(541, 274);
+            Controls.Add(btnRegistrar);
+            Controls.Add(labelCantViajes);
+            Controls.Add(label1);
+            Controls.Add(listBoxPatentes);
+            Controls.Add(btnSalir);
+            Controls.Add(btnResumenFinal);
+            Controls.Add(btnCargarViaje);
+            Name = "Form1";
+            Text = "Principal";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnCargarViaje;
+        private Button btnResumenFinal;
+        private Button btnSalir;
+        private Label label1;
+        public ListBox listBoxPatentes;
+        public Label labelCantViajes;
+        private Button btnRegistrar;
+    }
+}
---
+++ Ejercicio4/Fresumen.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Ejercicio4
+{
+    partial class Fresumen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBoxResumen = new ListBox();
+            btnSalir = new Button();
+            SuspendLayout();
+            // 
+            // listBoxResumen
+            // 
+            listBoxResumen.FormattingEnabled = true;
+            listBoxResumen.ItemHeight = 15;
+            listBoxResumen.Location = new Point(12, 12);
+            listBoxResumen.Name = "listBoxResumen";
+            listBoxResumen.Size = new Size(272, 184);
+            listBoxResumen.TabIndex = 0;
+            // 
+            // btnSalir
+            // 
+            btnSalir.DialogResult = DialogResult.OK;
+            btnSalir.Location = new Point(102, 202);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(75, 23);
+            btnSalir.TabIndex = 1;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // Fresumen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(293, 233);
+            Controls.Add(btnSalir);
+            Controls.Add(listBoxResumen);
+            Name = "Fresumen";
+            Text = "Fresumen";
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button btnSalir;
+        public ListBox listBoxResumen;
+    }
+}
